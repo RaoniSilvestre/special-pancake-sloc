8bee3a8f6b835aa1f4bcc00c2c6ed20eec7926b3
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-10-13 14:57:48 -0300

    refactor: Cleaning main

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
-use search::display::print_info;
-use search::search::search_tree;
-use search::structs::FileInfo;
-
-use std::collections::HashMap;
-
-mod cli;
-mod search;
+use sloc_rust::{config::Configuration, display::Display, search::Searcher};
 
 fn main() {
-    let matches = cli::get_opts();
-
-    match matches.get_one::<String>("PATH") {
-        Some(path) => {
-            let mut informações: HashMap<String, FileInfo> = HashMap::new();
-
-            search_tree(path, &mut informações, matches.get_flag("recursive"));
-            print_info(&informações);
-        }
-        None => {
-            panic!("<PATH> necessário para rodar programa!")
-        }
-    }
+    let configuration = Configuration::configure();
+    let mut seacher = Searcher::new(configuration);
+    seacher.search();
+    Display::print_info(seacher.result);
 }
