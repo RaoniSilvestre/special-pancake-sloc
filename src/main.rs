ec1c58ded1835549a9be85ace7a1635622596515
RaoniSilvestre <raoni.silvestre@hotmail.com>
2024-10-14 20:00:44 -0300

    Apenas atualizando nomes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use sloc_rust::{config::Configuration, display::Display, search::Searcher};
+use sloc_rust::{config::Configuration, display::Tabela, search::Searcher};
 
 fn main() {
     let configuration = Configuration::configure();
     let mut seacher = Searcher::new(configuration);
     seacher.search();
-    Display::print_info(seacher.result);
+    Tabela::print_info(seacher.result);
 }
